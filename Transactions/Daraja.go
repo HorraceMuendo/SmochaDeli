2b28f26906d3ef4f128737fcb4e44baabc8d7a94
HorraceMuendo <horracemuendo34@gmail.com>
2023-03-23 00:36:15 +0300

    Succesfully captured the token

--- Transactions/Daraja.go
+++ Transactions/Daraja.go
@@ -13,6 +13,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func getPassword() string {
+	//passphrase generated from smochadeliveryapp
+	passphrase := "ConquestpDefendvHarvestrHotp9"
+	envCode := os.Getenv("ENVCODE")
+	data := passphrase + envCode
+	hash := sha256.Sum256([]byte(data))
+	password := hex.EncodeToString(hash[:])
+	return password
+}
+func TimeStamp() string {
+	return time.Now().UTC().Format("20060102150405")
+}
+
 func DarajaApi(c *fiber.Ctx) error {
 	BASE_API := "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
 	TOKEN_API := "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
@@ -33,22 +46,8 @@ func DarajaApi(c *fiber.Ctx) error {
 	Consumerkey := os.Getenv("CONSUMERKEY")
 	consumerSecret := os.Getenv("CONSUMERSECRET")
 	Auth := consumerSecret + ":" + Consumerkey
-	AuthEncode := base64.StdEncoding.EncodeToString([]byte(Auth))
 
 	//req body json
-
-	RequestBody := fmt.Sprintf(`{
-	"ShortBusinessCode": "%s",
-	"Amount":"%s",
-	"PhoneNumber" :"%s",
-	"AccountReference":"%s",
-	"CallBackURL":"%s",
-	"TransactionDesc":"%s",
-	"Timestamp":"%s",
-	"Password":"%s"
-
-
-}`, ShortBusinessCode, Amount, PhoneNumber, AccountReference, CallBackURL, TransactionDesc, TimeStamp(), getPassword())
 
 	// creating the http request
 
@@ -62,14 +61,6 @@ func DarajaApi(c *fiber.Ctx) error {
 
 	req.SetBasicAuth(Consumerkey, consumerSecret)
 
-	//set-up the request headers
-	//req.Header.Add("Authorization", "Basic"+AuthEncode)
-	//req.Header.Add("Authorization", "Bearer"+)
-	//req.Header.Add("Content-Type", "application/json")
-	//req.Header.Add("Api_", "application/json")
-	// req.Header.Add("Accept", "application/json")
-	// req.Header.Add("Cache-Control", "no-cache")
-	// sending the http req
 	client := &http.Client{}
 	response, err := client.Do(req)
 	if err != nil {
@@ -92,19 +83,35 @@ func DarajaApi(c *fiber.Ctx) error {
 
 	fmt.Println(string(body))
 
+	//authenticaton encoding
+	AuthEncode := base64.StdEncoding.EncodeToString([]byte(Auth))
+
+	//writing the post request
+	RequestBody := fmt.Sprintf(`{
+		"ShortBusinessCode": "%s",
+		"Amount":"%s",
+		"PhoneNumber" :"%s",
+		"AccountReference":"%s",
+		"CallBackURL":"%s",
+		"TransactionDesc":"%s",
+		"Timestamp":"%s",
+		"Password":"%s"
+	
+	
+	}`, ShortBusinessCode, Amount, PhoneNumber, AccountReference, CallBackURL, TransactionDesc, TimeStamp(), getPassword())
+
+	//***setting up the request headers***
+
+	//set-up the request headers
+	//req.Header.Add("Authorization", "Basic"+AuthEncode)
+	//req.Header.Add("Authorization", "Bearer"+)
+	//req.Header.Add("Content-Type", "application/json")
+	//req.Header.Add("Api_", "application/json")
+	// req.Header.Add("Accept", "application/json")
+	// req.Header.Add("Cache-Control", "no-cache")
+	// sending the http req
+
 	return c.Status(200).JSON(fiber.Map{
 		"respbody": body,
 	})
-}
-func getPassword() string {
-	//passphrase generated from smochadeliveryapp
-	passphrase := "ConquestpDefendvHarvestrHotp9"
-	envCode := os.Getenv("ENVCODE")
-	data := passphrase + envCode
-	hash := sha256.Sum256([]byte(data))
-	password := hex.EncodeToString(hash[:])
-	return password
-}
-func TimeStamp() string {
-	return time.Now().UTC().Format("20060102150405")
 }
